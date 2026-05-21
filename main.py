56114766e3ddd425ed6d0b441ccec3d8c6438c18
Nomellime123456789 <stasiva555@gmail.com>
2026-05-21 13:17:21 +0300

    функция: добавить процент побед в зависимости от карты

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src.tracker import init_csv, add_match
+from src.analyzer import show_stats
+from src.visualizer import show_charts
 
 def main():
     init_csv()
@@ -6,12 +8,18 @@ def main():
     while True:
         print("\n=== CS2 Match Tracker ===")
         print("1. Добавить матч")
+        print("2. Показать статистику")
+        print("3. Показать графики")
         print("0. Выход")
 
         choice = input("\nВыбери действие: ").strip()
 
         if choice == "1":
             add_match()
+        elif choice == "2":
+            show_stats()
+        elif choice == "3":
+            show_charts()
         elif choice == "0":
             print("До встречи!")
             break
